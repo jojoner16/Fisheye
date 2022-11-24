9621edece66d371e83a513d6de01a9ee8e93bb42
jojoner16 <laybats.jonathan@orange.fr>
2022-11-24 13:50:02 +0100

    modification du fichier pour l'affichage des images et noms des photographes

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,13 @@
 async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({photographers: [...photographers]})
+
+    // Récupère les données des photographes depuis le fichier JSON
+    return fetch('data/photographers.json')
+        .then(response => response.json());
+
 }
 
 async function displayData(photographers) {
+
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
@@ -32,12 +15,16 @@ async function displayData(photographers) {
         const userCardDOM = photographerModel.getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
     });
+
 };
 
 async function init() {
-    // Récupère les datas des photographes
+
+    // Récupère les données des photographes pour ensuite les insérer dans index.html
     const { photographers } = await getPhotographers();
     displayData(photographers);
+    
 };
-
+    
 init();
+    
